c8e07a22a18e3946d3865277b1ea714ea51704a7
Your Windows Name <your.email@example.com>
2024-03-06 22:34:23 +0300

    from  lapi

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -9,6 +9,51 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: [
           AppBarWidget(),
+          // SEARCH
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical:10,
+              horizontal: 15,
+            ),
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 3),
+                  ),
+                ]
+              ),
+            child: Padding: (
+              padding: EdgeInsets.symmetric(
+              horizontal: 10,
+              ),
+              child: Row( children: [
+                Icon(CupertinoIcons.search, color: Colors.red),
+                Container(
+                  height: 50,
+                  width: 300,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      ),
+                    child: TextFormField(),
+                    decoration: InputDecoration(
+                      hintText: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ],),
+            ),
+          ),
+
+
         ],
       ),
     );
--- lib/Widgets/AppBarWidget.dart
+++ lib/Widgets/AppBarWidget.dart
@@ -6,24 +6,31 @@ class AppBarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-      child: Row(
+      child: Row( 
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [InkWell(
-          onTap: () {},
-          child: Container(
-            padding: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [BoxShadow(
-                blurRadius: 10,
-                offset: Offset(0, 3),
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 2,)
-              ],
+        // children: [InkWell(
+        children: [
+          InkWell(
+            onTap: () {},
+            child: Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Icon(CupertinoIcons.bars),
             ),
-            child: Icon(CupertinoIcons.bars),
           ),
-        )],),
+
+        ],),
     );
   }
 }
