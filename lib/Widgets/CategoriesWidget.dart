f38d09dadaa61ad0ff25886301fbe59bb801435b
linux_developer <philipaswa01@gmail.com>
2024-03-07 13:54:25 +0300

    commit 1.2

--- lib/Widgets/CategoriesWidget.dart
+++ lib/Widgets/CategoriesWidget.dart
@@ -8,17 +8,31 @@ class CategoriesWidget extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
         child: Row(children: [
-          Padding(padding: EdgeInsets.symmetric(horizontal: 10),
-          child: Container(
-            padding: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
+          // SINGLE ITEM
+          for (int i = 0; i < 10; i++)
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    )
+                  ]),
+              child: Image.asset(
+                "images/logo_1.png",
+                width: 50,
+                height: 50,
+              ),
             ),
-            child: Image.asset("images/logo_1.png"),
           ),
-          )
-        ],),
+        ]),
       ),
     );
   }
