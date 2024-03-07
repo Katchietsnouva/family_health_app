86752ca80e747e1c4685c9454f3906664356ec41
linux_developer <philipaswa01@gmail.com>
2024-03-07 13:35:56 +0300

    commit 1.0

--- lib/Widgets/CategoriesWidget.dart
+++ lib/Widgets/CategoriesWidget.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 class CategoriesWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView()
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+        child: Row(children: [
+          Padding(padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            ),
+            child: Image.asset("images/"),
+          ),
+          )
+        ],),
+      ),
+    );
   }
 }
