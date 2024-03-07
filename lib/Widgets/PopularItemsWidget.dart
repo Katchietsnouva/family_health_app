043c750c1bfc09690fa7612471aea24bbf35dad8
linux_developer <philipaswa01@gmail.com>
2024-03-07 16:59:06 +0300

    commit 0.34

--- lib/Widgets/PopularItemsWidget.dart
+++ lib/Widgets/PopularItemsWidget.dart
@@ -9,13 +9,15 @@ class PopularItemsWidget extends StatelessWidget {
         padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
         child: Row(
           children: [
+
+
             // SINGLE ITEM
-            // for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 7),
               child: Container(
                 width: 170,
-                height: 225,
+                height: 260,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(10),
@@ -45,19 +47,48 @@ class PopularItemsWidget extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                       ),),
                       SizedBox(height: 4),
-                      Text("Have the best of out arts",
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),),
+
+
+                      // Scrollable text
+                      Container(
+                        height: 50, // Set a fixed height or use constraints
+                        child: SingleChildScrollView(
+                          child: Text(
+                            "Have the best of our arts, a really long description that may not fit in a single line and should be scrollable if it exceeds the available space.",
+                            style: TextStyle(
+                              fontSize: 10,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      // Text("Have the best of out arts",
+                      // style: TextStyle(
+                      //   fontSize: 10,
+                      // ),),
+
+
                       SizedBox(height: 12),
-
-                    ],
-
-
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("\$10", style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                          Icon(Icons.favorite_border_outlined,
+                          color: Colors.red,
+                          size: 26,)
+                        ],
+                      )
+                    ],                    
                   ),
                 ),
               ),
             ),
+
+
 
 
         ]),
@@ -66,3 +97,181 @@ class PopularItemsWidget extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// class PopularItemsWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 225, // Set a fixed height or use constraints
+//       child: ListView.builder(
+//         scrollDirection: Axis.horizontal,
+//         itemCount: 10,
+//         itemBuilder: (context, index) {
+//           return Padding(
+//             padding: EdgeInsets.symmetric(horizontal: 7),
+//             child: Container(
+//               width: 170,
+//               decoration: BoxDecoration(
+//                 color: Colors.white,
+//                 borderRadius: BorderRadius.circular(10),
+//                 boxShadow: [
+//                   BoxShadow(
+//                     color: Colors.grey.withOpacity(0.5),
+//                     spreadRadius: 2,
+//                     blurRadius: 10,
+//                     offset: Offset(0, 3),
+//                   )
+//                 ],
+//               ),
+//               child: Padding(
+//                 padding: EdgeInsets.symmetric(horizontal: 10),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Container(
+//                       child: Image.asset("images/popular/logo_1.png"),
+//                       height: 130,
+//                     ),
+//                     Text(
+//                       "Hot Graphiti",
+//                       style: TextStyle(
+//                         fontSize: 10,
+//                         fontWeight: FontWeight.bold,
+//                       ),
+//                     ),
+//                     SizedBox(height: 4),
+//                     Text(
+//                       "Have the best of our arts",
+//                       style: TextStyle(
+//                         fontSize: 10,
+//                       ),
+//                     ),
+//                     SizedBox(height: 12),
+//                     Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                       children: [
+//                         Text(
+//                           "\$10",
+//                           style: TextStyle(
+//                             fontSize: 17,
+//                             color: Colors.red,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                         Icon(
+//                           Icons.favorite_border_outlined,
+//                           color: Colors.red,
+//                           size: 26,
+//                         )
+//                       ],
+//                     )
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// class PopularItemsWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 225, // Set a fixed height or use constraints
+//       child: ListView.builder(
+//         scrollDirection: Axis.horizontal,
+//         itemCount: 3,
+//         itemBuilder: (context, index) {
+//           return Padding(
+//             padding: EdgeInsets.symmetric(horizontal: 7),
+//             child: Container(
+//               width: 170,
+//               decoration: BoxDecoration(
+//                 color: Colors.white,
+//                 borderRadius: BorderRadius.circular(10),
+//                 boxShadow: [
+//                   BoxShadow(
+//                     color: Colors.grey.withOpacity(0.5),
+//                     spreadRadius: 2,
+//                     blurRadius: 10,
+//                     offset: Offset(0, 3),
+//                   )
+//                 ],
+//               ),
+//               child: Padding(
+//                 padding: EdgeInsets.symmetric(horizontal: 10),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Container(
+//                       child:
+//                           Image.asset("images/popular/logo_${index + 1}.png"),
+//                       height: 130,
+//                     ),
+//                     Text(
+//                       "Hot Graphiti",
+//                       style: TextStyle(
+//                         fontSize: 10,
+//                         fontWeight: FontWeight.bold,
+//                       ),
+//                     ),
+//                     SizedBox(height: 4),
+//                     Text(
+//                       "Have the best of our arts",
+//                       style: TextStyle(
+//                         fontSize: 10,
+//                       ),
+//                     ),
+//                     SizedBox(height: 12),
+//                     Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                       children: [
+//                         Text(
+//                           "\$10",
+//                           style: TextStyle(
+//                             fontSize: 17,
+//                             color: Colors.red,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                         Icon(
+//                           Icons.favorite_border_outlined,
+//                           color: Colors.red,
+//                           size: 26,
+//                         )
+//                       ],
+//                     )
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
