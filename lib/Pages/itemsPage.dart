cf81d53bd15d5e7dfaf95c7d050d5d0ff1ee235b
linux_developer <philipaswa01@gmail.com>
2024-03-08 23:39:45 +0300

    commit major 0.70

---
+++ lib/Pages/itemsPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class itemsPage extends StatelessWidget {
+  final List<String> imagePaths =
+      List.generate(3, (index) => "images/newest/logo_${index + 1}.png");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // scrollDirection: Axis.horizontal,
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: ListView(children: [
+          
+        ]),
+      ),
+    );
+  }
+}
