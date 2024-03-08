068df2712950b49b3da177054351485b2c615bb0
linux_developer <philipaswa01@gmail.com>
2024-03-09 00:44:47 +0300

    commit major 0.74

--- lib/Pages/ItemsPage.dart
+++ lib/Pages/ItemsPage.dart
@@ -1,6 +1,9 @@
 import 'package:family_health_app/Widgets/AppBarWidget.dart';
 import 'package:flutter/material.dart';
 
+import 'package:clippy_flutter/clippy_flutter.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
 class ItemsPage extends StatelessWidget {
   final List<String> imagePaths =
       List.generate(3, (index) => "images/newest/logo_${index + 1}.png");
@@ -19,9 +22,42 @@ class ItemsPage extends StatelessWidget {
               child: Image.asset(
                 "images/popular/logo_1.png",
                 // width: 50,
-                height: 130,
+                height: 300 ,
               ),
-            )
+            ),
+            Arc(
+              edge: Edge.TOP,
+              arcType: ArcType.CONVEY,
+              height: 30, 
+              child: Container(
+                width: double.infinity,
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RatingBar.builder(
+                      initialRating: 4,
+                      minRating: 1,
+                      direction:Axis.horizontal, 
+                      itemCount: 5,
+                      itemSize: 18,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 4),
+                      itemBuilder: (context, _) => Icon(Icons.star, color: Colors.red),
+                      onRatingUpdate: (index){},
+                    ),
+                    Text("\$10", 
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                    
+
+
+                  ],
+                ),
+              ),
+            ),
 
         ]),
       ),
