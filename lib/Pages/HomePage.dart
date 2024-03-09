f74ad2838c3c6186a0475725762511868d88ad50
linux_developer <philipaswa01@gmail.com>
2024-03-09 17:23:40 +0300

    commit 1.11

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,97 +1,102 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
 
-import '../Widgets/DrawerWidget.dart';
-import '../Widgets/SearchWidget.dart';
-import '../Widgets/AppBarWidget.dart';
-import '../Widgets/ServicesWidget.dart';
-import '../Widgets/PopularItemsWidget.dart';
-import '../Widgets/NewestItemsWidget.dart';
+// import '../Widgets/DrawerWidget.dart';
+// import '../Widgets/SearchWidget.dart';
+// import '../Widgets/AppBarWidget.dart';
+// import '../Widgets/ServicesWidget.dart';
+// import '../Widgets/PopularItemsWidget.dart';
+// import '../Widgets/NewestItemsWidget.dart';
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          AppBarWidget(),
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: ListView(
+//         children: [
+//           AppBarWidget(),
 
-          // SEARCH
-          SearchWidget(),
+//           // SEARCH
+//           SearchWidget(),
 
-          // SERVICE CATEGOIES
-          Padding(
-            padding: EdgeInsets.only(top: 20, left: 10),
-            child: Text(
-              "Services",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-          ),
+//           // DETAILS SECTION
+//           Padding(
+//             padding: EdgeInsets.only(top: 20, left: 10),
+//             child: Text(
+//               "Details",
+//               style: TextStyle(
+//                 fontWeight: FontWeight.bold,
+//                 fontSize: 20,
+//               ),
+//             ),
+//           ),
 
-          ServicesWidget(),
+//           // SERVICE CATEGOIES (Tab 1)
+//           ServicesWidget(),
 
-          // POPULAR WIDGET
-          Padding(
-            padding: EdgeInsets.only(top: 20, left: 10),
-            child: Text(
-              "Popular",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-          ),
+//           //location tab (tab 2)
+//           locationWidget(),
 
-          PopularItemsWidget(),
 
-          // NEWEST ARRRIVAL WIDGET
-          Padding(
-            padding: EdgeInsets.only(top: 20, left: 10),
-            child: Text(
-              "newest",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-          ),
+//           // POPULAR WIDGET
+//           Padding(
+//             padding: EdgeInsets.only(top: 20, left: 10),
+//             child: Text(
+//               "Popular",
+//               style: TextStyle(
+//                 fontWeight: FontWeight.bold,
+//                 fontSize: 20,
+//               ),
+//             ),
+//           ),
 
-          NewestItemsWidget(),
-        ],
-      ),
+//           PopularItemsWidget(),
 
-      // DRAWER
-      drawer: DrawerWidget(),
-      floatingActionButton: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 3,
-                blurRadius: 10,
-                offset: Offset(0, 3),
-              )
-            ]),
-        child: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, "cartPage");
-          },
-          child: Icon(
-            CupertinoIcons.cart,
-            size: 28,
-            color: Colors.red,
-          ),
-          backgroundColor: Colors.white,
-        ),
-      ),
-    );
-  }
-}
+//           // NEWEST ARRRIVAL WIDGET
+//           Padding(
+//             padding: EdgeInsets.only(top: 20, left: 10),
+//             child: Text(
+//               "Newest",
+//               style: TextStyle(
+//                 fontWeight: FontWeight.bold,
+//                 fontSize: 20,
+//               ),
+//             ),
+//           ),
+
+//           NewestItemsWidget(),
+//         ],
+//       ),
+
+//       // DRAWER
+//       drawer: DrawerWidget(),
+//       floatingActionButton: Container(
+//         decoration: BoxDecoration(
+//             color: Colors.white,
+//             borderRadius: BorderRadius.circular(20),
+//             boxShadow: [
+//               BoxShadow(
+//                 color: Colors.grey.withOpacity(0.5),
+//                 spreadRadius: 3,
+//                 blurRadius: 10,
+//                 offset: Offset(0, 3),
+//               )
+//             ]),
+//         child: FloatingActionButton(
+//           onPressed: () {
+//             Navigator.pushNamed(context, "cartPage");
+//           },
+//           child: Icon(
+//             CupertinoIcons.cart,
+//             size: 28,
+//             color: Colors.red,
+//           ),
+//           backgroundColor: Colors.white,
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
 
@@ -233,3 +238,121 @@ class HomePage extends StatelessWidget {
 
 
 
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../Widgets/DrawerWidget.dart';
+import '../Widgets/SearchWidget.dart';
+import '../Widgets/AppBarWidget.dart';
+import '../Widgets/ServicesWidget.dart';
+import '../Widgets/LocationWidget.dart';  /
+import '../Widgets/PopularItemsWidget.dart';
+import '../Widgets/NewestItemsWidget.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2, // Number of tabs
+      child: Scaffold(
+        body: ListView(
+          children: [
+            AppBarWidget(),
+
+            // SEARCH
+            SearchWidget(),
+
+            // DETAILS SECTION
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 10),
+              child: Text(
+                "Details",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+            // TAB BAR
+            TabBar(
+              tabs: [
+                Tab(text: 'Services'),  // First tab
+                Tab(text: 'Location'),  // Second tab
+              ],
+            ),
+
+            // TAB BAR VIEW
+            Container(
+              height: 0,  // Set the height for tab content
+              child: TabBarView(
+                children: [
+                  // SERVICE CATEGORIES (Tab 1)
+                  ServicesWidget(),
+
+                  // LOCATION TAB (Tab 2)
+                  LocationWidget(),
+                ],
+              ),
+            ),
+
+            // POPULAR WIDGET
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 10),
+              child: Text(
+                "Popular",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+            PopularItemsWidget(),
+
+            // NEWEST ARRIVAL WIDGET
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 10),
+              child: Text(
+                "Newest",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+            NewestItemsWidget(),
+          ],
+        ),
+
+        // DRAWER
+        drawer: DrawerWidget(),
+        floatingActionButton: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 3,
+                  blurRadius: 10,
+                  offset: Offset(0, 3),
+                )
+              ]),
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "cartPage");
+            },
+            child: Icon(
+              CupertinoIcons.cart,
+              size: 28,
+              color: Colors.red,
+            ),
+            backgroundColor: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
