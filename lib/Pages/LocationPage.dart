dbbb038e760b49fcce8182bf2d9fb7c6530939df
linux_developer <philipaswa01@gmail.com>
2024-03-09 18:32:08 +0300

    commit 1.184

--- lib/Pages/LocationPage.dart
+++ lib/Pages/LocationPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class LocationPage extends StatelessWidget {
   final List<String> imagePaths =
@@ -9,46 +8,41 @@ class LocationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      // scrollDirection: Axis.horizontal,
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        child: Column(
-          children: [
-            
-
-            // SINGLE ITEM
-            // for (int i = 0; i < 10; i++)
-            // Display all items
-            for (int i = 0; i < imagePaths.length; i++)
-            Padding(
+        child: ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: imagePaths.length,
+          itemBuilder: (BuildContext context, int i) {
+            return Padding(
               padding: EdgeInsets.symmetric(vertical: 10),
               child: Container(
                 width: 380,
                 height: 150,
-                // padding: EdgeInsets.all(8),
                 decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 3,
-                        blurRadius: 10,
-                        offset: Offset(0, 3),
-                      )
-                    ],),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
                 child: Row(
                   children: [
                     InkWell(
-                      onTap: () {},                      
+                      onTap: () {},
                       child: Container(
                         alignment: Alignment.center,
                         padding: EdgeInsets.all(8),
                         child: Image.asset(
-                        imagePaths[i],
-                        // "images/newest/logo_1.png",
-                        width: 120,
-                        height: 150,
+                          imagePaths[i],
+                          width: 120,
+                          height: 150,
                         ),
                       ),
                     ),
@@ -65,17 +59,117 @@ class LocationPage extends StatelessWidget {
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ]
+                        ],
                       ),
                     ),
-                  ]
+                  ],
                 ),
               ),
-            ),
-          ]
+            );
+          },
         ),
       ),
     );
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+// class LocationPage extends StatelessWidget {
+//   final List<String> imagePaths =
+//       List.generate(14, (index) => "images/location/location_${index + 1}.jpeg");
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       // scrollDirection: Axis.horizontal,
+//       child: Padding(
+//         padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+//         child: Column(
+//           children: [
+            
+
+//             // SINGLE ITEM
+//             // for (int i = 0; i < 10; i++)
+//             // Display all items
+//             for (int i = 0; i < imagePaths.length; i++)
+//             Padding(
+//               padding: EdgeInsets.symmetric(vertical: 10),
+//               child: Container(
+//                 width: 380,
+//                 height: 150,
+//                 // padding: EdgeInsets.all(8),
+//                 decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     borderRadius: BorderRadius.circular(10),
+//                     boxShadow: [
+//                       BoxShadow(
+//                         color: Colors.grey.withOpacity(0.5),
+//                         spreadRadius: 3,
+//                         blurRadius: 10,
+//                         offset: Offset(0, 3),
+//                       )
+//                     ],),
+//                 child: Row(
+//                   children: [
+//                     InkWell(
+//                       onTap: () {},                      
+//                       child: Container(
+//                         alignment: Alignment.center,
+//                         padding: EdgeInsets.all(8),
+//                         child: Image.asset(
+//                         imagePaths[i],
+//                         // "images/newest/logo_1.png",
+//                         width: 120,
+//                         height: 150,
+//                         ),
+//                       ),
+//                     ),
+//                     Container(
+//                       width: 170,
+//                       child: Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                         children: [
+//                           Text(
+//                             "Hot Wall Neons",
+//                             style: TextStyle(
+//                               fontSize: 15,
+//                               fontWeight: FontWeight.bold,
+//                             ),
+//                           ),
+//                         ]
+//                       ),
+//                     ),
+//                   ]
+//                 ),
+//               ),
+//             ),
+//           ]
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+// // It seems like you have a common issue when using SingleChildScrollView with a column of widgets that contains InkWell and GestureDetector widgets, especially when they are placed inside ListView or Column and wrapped with a SingleChildScrollView. This often results in rendering errors due to a conflict in gestures.
+
+// // To fix this, you can try using ListView.builder instead of a Column with a loop. Here's an updated version of your code:
+
+// // This approach ensures that the SingleChildScrollView is used at the root level, and the ListView.builder takes care of the list items. Additionally, I've set physics to NeverScrollableScrollPhysics() for the ListView.builder to avoid conflicts with the outer SingleChildScrollView.
